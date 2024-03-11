99a9276bde8cb75d1dd5199737a27e8de4da4245
Sanjib Adhya <sanjib@zacksresearch.in>
2024-03-11 06:39:16 -0500

    added practice sessions for JS

---
+++ js-refresher/src/main.js
@@ -0,0 +1,65 @@
+//using switch statement
+
+//what is the day
+let day
+
+switch(new Date().getDay()){
+    case 0:
+        day = "Sunday"
+        break
+    case 1:
+        day = "Monday"
+        break
+    case 2:
+        day = "Tuesday"
+        break
+    case 3:
+        day = "Wednesday"
+        break
+}
+
+document.querySelector(".today").innerHTML = day
+
+//fruits
+let fruit = "Apple";
+
+switch(fruit){
+    case "Banana":
+        fruit = "Banana"
+        break
+    case "Apple":
+        fruit = "Apple"
+        break
+
+    default:
+        fruit = "Berries"
+}
+
+document.querySelector(".fruit").innerHTML = fruit
+
+//date object
+const d = new Date("1970-01-02")
+
+document.querySelector(".date").innerHTML = d
+
+//output date as a name
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+
+const date = new Date()
+
+const today = days[date.getDay()]
+
+document.getElementById("today").innerHTML = today
+
+//output current month as name
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+const date2 = new Date()
+
+const currMonth = months[date2.getMonth()]
+
+document.getElementById("month").innerHTML = "the current month is: " + currMonth
+
+//find out the current age of a person
+
+
